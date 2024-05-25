d7d2932824e2ab2f971dc1fab49e3c12f7c166f6
Joseamat25 <jalmat2509@gmail.com>
2024-05-25 12:14:34 +0200

    Scripts: added some example

--- prueba.py
+++ prueba.py
@@ -5,7 +5,7 @@ url = "http://127.0.0.1:8000//inference_cross_encoder"
 # Datos a enviar en la petición
 data = {
     "question": "This is an example test?",
-    "total_chunks": ["Example chunk for dealing this case", "This is another case", "Yes, of course, this is an example test"*1000000]
+    "total_chunks": ["Example chunk for dealing this case", "This is another case", "Yes, of course, this is an example test"]
 }
 
 # Enviar petición POST
